df5c7054ae44f7f7f74b4983da83d9a1610a7224
Henrik Kjelsberg <hkjels@me.com>
2013-09-14 19:14:53 +0200

    Added some basic debugging

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 
 var express = require('express')
   , request = require('superagent')
+  , debug = require('debug')
   , app = module.exports = express();
 
 
@@ -25,17 +26,23 @@ app.use(app.router);
 
 var weather = 'http://www.weatherlink.com/user/kjero/index.php?view=summary&headers=1';
 app.get('/weather', function(req, res) {
+  debug('Request made');
   request.get(weather).end(function(resp) {
-    if (res.error) console.error(res.error);
-    var text = resp.text
-      , start = text.indexOf('<!-- START: SUMMARY WEATHER DISPLAY -->')
-      , end = text.indexOf('<!-- END: SUMMARY WEATHER DISPLAY -->');
-    text = text.substring(start, end);
-    text = text.replace(/\<tr.*\<img.*tr\>/g, '');
-    text = text.replace(/width..[^"]+"/g, '');
-    text = text.replace(/\<hr.+\>/g, '');
-    res.type('html');
-    res.end(text);
+    if (res.error) {
+      debug('Failed request');
+      debug(res.error);
+      console.error(res.error);
+    } else {
+      var text = resp.text
+        , start = text.indexOf('<!-- START: SUMMARY WEATHER DISPLAY -->')
+        , end = text.indexOf('<!-- END: SUMMARY WEATHER DISPLAY -->');
+      text = text.substring(start, end);
+      text = text.replace(/\<tr.*\<img.*tr\>/g, '');
+      text = text.replace(/width..[^"]+"/g, '');
+      text = text.replace(/\<hr.+\>/g, '');
+      res.type('html');
+      res.end(text);
+    }
   });
 });
 
